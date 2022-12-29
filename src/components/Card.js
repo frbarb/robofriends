5aa830e685dc0e808f1d6db2fabf602dd18972e4
frbar <felipe.barbosa@gmail.com>
2022-12-29 11:08:18 -0300

    Error Boundry changes

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from "react";
+
+const Card = ({ name, email, id }) => {    
+    return(
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow shadow-5">            
+            <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
+            <>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </>                
+        </div>
+    );
+}
+
+export default Card;
